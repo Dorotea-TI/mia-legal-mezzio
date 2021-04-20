031d9f4a503fc502e5c8dd00ba0e3712d8032937
Matias Camiletti <matias.camiletti@gmail.com>
2021-04-20 12:10:56 -0300

    Add angular files.

---
+++ data/angular/entities/mia_legal.ts
@@ -0,0 +1,8 @@
+export class MiaLegal {
+    id: number = 0;
+    slug: string = '';
+    title: string = '';
+    content: any = {};
+    created_at: string = '';
+    last_revision: string = '';
+}
